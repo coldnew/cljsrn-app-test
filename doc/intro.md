22cd22b08aa2a64f3ac1171256bb13ce461512ff
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-03-11 22:49:41 +0800

    first commit
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljsrnapp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cljsrnapp/core_test.clj
@@ -0,0 +1,7 @@
+(ns cljsrnapp.core-test
+  (:require [clojure.test :refer :all]
+            [cljsrnapp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
